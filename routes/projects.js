9e31f548a9a62abf48750a50bba9ec9dad856b5c
Caroline Leopold <caroline.leopold@gmail.com>
2019-03-20 12:23:57 -0400

    remove routes add jg route

--- routes/projects.js
+++ routes/projects.js
@@ -5,6 +5,16 @@ router.get('/', function(req, res, next) {
   res.render('projects', { title: 'Here\'s Our Work', layout: 'portfolio' });
 });
 
+router.get('/jeff-grant-influencer', function(req, res, next) {
+  res.render('jeff-grant-influencer', { 
+    title: 'Magaziney Title.',
+    name: 'Jeff Grant',
+    jobTitle: 'Videographer, YouTube Influencer',
+    caption: 'Jeff Grant on location for a video shoot',
+    layout: 'case' 
+  });
+});
+
 router.get('/health', function(req, res, next) {
   res.render('health', { 
     title: 'Relatable Content for Household Health Brands',
@@ -15,36 +25,5 @@ router.get('/health', function(req, res, next) {
     layout: 'case' 
   });
 });
-
-router.get('/lead-generation', function(req, res, next) {
-  res.render('lead-generation', { 
-    title: 'Lead generation in a high stakes market',
-    client: 'Comodo',
-    product1: "white paper",
-    product2: "research",
-    tag: [ "content strategy", "white paper", "lead generation"
-          ],
-    layout: 'case' 
-  });
-});
-
-router.get('/websites', function(req, res, next) {
-  res.render('websites', { 
-    title: 'Strategy on Healthcare Marketing',
-    product1: "web content",
-    product2: "SEO",
-    tag1: 'web copy',
-    tag2: 'strategy',
-    tag3: 'SEO',
-    layout: 'case' 
-  });
-});
-
-
-
-
-  router.get('/marketing', function(req, res, next) {
-    res.render('marketing', { title: 'Marketing' });
-  });
 
   module.exports = router;
