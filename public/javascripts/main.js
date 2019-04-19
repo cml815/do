f3fb8e6c3b77ad37a781bfdfcd23157568392d80
Caroline Leopold <caroline.leopold@gmail.com>
2019-04-18 22:34:29 -0400

    homepage

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,6 +19,39 @@ function stickyNavigation() {
 
 window.addEventListener('scroll', stickyNavigation);
 
+/* Animate home page image */
+
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'fade-in-element'
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
