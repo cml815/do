dae0d5df19ff226f7b8e0c31623d722e2c9b2733
Caroline Leopold <caroline.leopold@gmail.com>
2019-05-02 17:38:38 -0400

    cta and contact

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,6 +19,17 @@ function stickyNavigation() {
 
 window.addEventListener('scroll', stickyNavigation);
 
+/* Nav toggle
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
+
+*/
+
 /* Hide logo on scroll 
 
 function scrollFunction() {
