816b7a1fff1e8ba758cf6db80dcc420b2c1f0e52
Caroline Leopold <caroline.leopold@gmail.com>
2019-07-21 12:58:09 -0400

    rm button styles

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,6 +19,21 @@ function stickyNavigation() {
 
 window.addEventListener('scroll', stickyNavigation);
 
+/* Shrink navigation */
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
 /* 
 setInterval(function () { 
   document.getElementById("colorful").style.background= '#'+Math.floor(Math.random()*16777215).toString(16); 
