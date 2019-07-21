4c4d0e4fdfb51e279660329e66bdacc3b7d5be57
Caroline Leopold <caroline.leopold@gmail.com>
2019-07-21 15:50:13 -0400

    change overlay to top-down

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,16 +19,16 @@ function stickyNavigation() {
 
 window.addEventListener('scroll', stickyNavigation);
 
-/* overlay nav */
+// Overlay menu //
 
-/* Open when someone clicks on the span element */
+/* Open */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.height = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+/* Close */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.height = "0%";
 }
 
 /* Shrink navigation */
