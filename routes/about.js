dba4e29e58cd95a0565d0c6b70c8c1a39d5831e8
Caroline Leopold <caroline.leopold@gmail.com>
2019-08-02 16:10:43 -0400

    work route

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('about', { layout: 'layout' });
+    res.render('about', { layout: 'default' });
   });
   
 router.get('/team-bios', function(req, res, next) {
---
+++ routes/work.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+
+/* Services routes */
+
+/* 
+router.get('/work', function(req, res, next) {
+  res.render('work', { title: 'Marketing and Fundraising Services | Fuel Copy', description: 'Fuel growth with websites, content, SEO and grant proposals', shortTitle: "What we offer", bodyClass: 'work'});
+});
+*/
+
+router.get('/', function(req, res, next) {
+  res.render('work', { title: 'Services | Fuel Copy', bodyClass: 'projects', shortTitle: 'Success stories', description: 'Lessons from our client success stories', layout: 'default' });
+});
+
+router.get('/growth-marketing', function(req, res, next) {
+  res.render('growth-marketing', { 
+    title: 'Digital Marketing',
+    summary: '',
+    name: '',
+    tag: ['content marketing','influencer marketing','video'],
+    layout: 'default' 
+  });
+});
+
+router.get('/media-relations', function(req, res, next) {
+  res.render('press-campaign', { 
+    title: 'Mass Shootings, No More',
+    summary: '',
+    tag: ['strategy', 'public relations', 'earned media'],
+    layout: 'default' 
+  });
+});
+
+// Interviews case study route
+
+router.get('/lead-generation', function(req, res, next) {
+  res.render('founder-stories', { 
+    title: 'Founder stories on risk, reward and travel',
+    summary: 'Fuel interviewed dozens of entrepreneurs on how to make a mark in the travel world',
+    name: 'null',
+    jobTitle: 'null',
+    caption: 'null',
+    credit: 'null',
+    tag: ['content marketing', 'thought leadership', 'seo'],
+    layout: 'case' 
+  });
+});
+
+// Grants route
+
+router.get('/grants', function(req, res, next) {
+  res.render('sbir-grants', { 
+    title: 'Win grant awards with federal and SBIR grant proposals',
+    summary: 'How Fuel helped tech and biotech companies win funding to bring their innovations to market',
+    name: 'null',
+    jobTitle: 'null',
+    caption: 'null',
+    credit: 'null',
+    tag: ['grants', 'sbir', 'biotech'],
+    layout: 'case' 
+  });
+});
+
+// Authority blogging - thought eadership case study
+
+router.get('/thought-leadership', function(req, res, next) {
+  res.render('thought-leadership', { 
+    title: 'Healthcare content readers can trust',
+    summary: 'Growth without dilution: Small business innovation research grants',
+    name: 'null',
+    jobTitle: 'null',
+    caption: 'null',
+    credit: 'null',
+    tag: ['thought leadership', 'magazine style', 'authority'],
+    layout: 'case' 
+  });
+});
+
+// Web design case study route
+
+router.get('/web-design', function(req, res, next) {
+  res.render('web-design', { 
+    title: 'Usable, useful websites',
+    summary: 'Web design to introduce new companies to discerning audiences',
+    name: 'null',
+    jobTitle: 'null',
+    caption: 'null',
+    credit: 'null',
+    tag: ['web design', 'prototyping', 'UI/UX'],
+    layout: 'case' 
+  });
+});
+
+
+
+/* CeasefirePA case study - Credit: David Scholnick
+
+router.get('/cease-fire-pa-strategy', function(req, res, next) {
+  res.render('cease-fire-pa-strategy'), {
+    title: 'Strategy to bring visibility on gun control legislation in Pennsylvania',
+    name: 'Shira Goodman',
+    jobTitle: 'Executive Director',
+    layout: 'case'
+  });
+});
+
+*/
+
+  module.exports = router;
