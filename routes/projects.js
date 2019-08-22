19fa6bb491223c8553ec9c160a3af14ae864f0f1
Caroline Leopold <caroline.leopold@gmail.com>
2019-08-22 01:02:14 -0400

    rebuild projects routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,134 +1,124 @@
 var express = require('express');
 var router = express.Router();
 
-/* Project's Page */
-
 router.get('/', function(req, res, next) {
-  res.render('projects', { title: 'Case studies of marketing and fundraising success', bodyClass: 'projects', shortTitle: 'Success stories', description: 'Lessons from our client success stories', layout: 'default' });
-});
-
-// Social media influencer case study route
-
-router.get('/social-media-influencer', function(req, res, next) {
-  res.render('social-media-influencer', { 
-    title: 'Small Channels, Mighty Influence',
-    bodyClass: 'grant-case-study',
-    summary: 'How young adults relate and emulate social media stars',
-    type: 'Case Study',
-    name: 'Jeff Grant',
-    jobTitle: 'Videographer, YouTube Influencer',
-    caption: 'Jeff Grant on location for a video shoot',
-    credit: 'RMM (get info)',
-    tag: ['social media','influencer marketing','video'],
-    layout: 'case' 
+    res.render('projects', { title: 'Case studies of marketing and fundraising success', bodyClass: 'projects', shortTitle: 'Success stories', description: 'Lessons from our client success stories', layout: 'default' });
   });
-});
 
-// PR case study route
+  router.get('/social-media-influencer', function(req, res, next) {
+    res.render('social-media-influencer', { 
+      title: 'Small Channels, Mighty Influence',
+      bodyClass: 'grant-case-study',
+      tag: ['social media','influencer marketing','video'],
+      layout: 'default' 
+    });
+  });
+
+  // PR case study route
 
 router.get('/press-campaign', function(req, res, next) {
-  res.render('press-campaign', { 
-    title: 'Mass Shootings, No More',
-    bodyClass: 'press-case-study',
-    summary: 'How a citizens group organized the largest gun violence prevention rally ever in Pennsylvania and was catapulted to the national stage.',
-    name: 'David Scholnick',
-    jobTitle: 'Videographer, YouTube Influencer',
-    caption: '',
-    credit: 'RMM (get info)',
-    tag: ['strategy', 'public relations', 'earned media'],
-    layout: 'case' 
+    res.render('press-campaign', { 
+      title: 'Mass Shootings, No More',
+      bodyClass: 'press-case-study',
+      summary: 'How a citizens group organized the largest gun violence prevention rally ever in Pennsylvania and was catapulted to the national stage.',
+      name: 'David Scholnick',
+      jobTitle: 'Videographer, YouTube Influencer',
+      caption: '',
+      credit: 'RMM (get info)',
+      tag: ['strategy', 'public relations', 'earned media'],
+      layout: 'default' 
+    });
   });
-});
 
-// Interviews case study route
+  // Interviews case study route
 
 router.get('/founder-stories', function(req, res, next) {
-  res.render('founder-stories', { 
-    title: 'Founder stories on risk, reward and travel',
-    bodyClass: 'founder-case-study',
-    description: 'Fuel interviewed dozens of entrepreneurs on how to make a mark in the travel world',
-    name: 'null',
-    jobTitle: 'null',
-    caption: 'null',
-    credit: 'null',
-    tag: ['content marketing', 'thought leadership', 'seo'],
-    layout: 'case' 
+    res.render('founder-stories', { 
+      title: 'Founder stories on risk, reward and travel',
+      bodyClass: 'founder-case-study',
+      description: 'Fuel interviewed dozens of entrepreneurs on how to make a mark in the travel world',
+      name: 'null',
+      jobTitle: 'null',
+      caption: 'null',
+      credit: 'null',
+      tag: ['content marketing', 'thought leadership', 'seo'],
+      layout: 'default' 
+    });
   });
-});
 
-// Grants route
+  // Grants route
 
 router.get('/sbir-grants', function(req, res, next) {
-  res.render('sbir-grants', { 
-    title: 'Growth without dilution: Small business innovation research grants',
-    bodyClass: 'sbir-case-study',
-    summary: 'How Fuel helped tech and biotech companies win funding to bring their innovations to market',
-    name: 'null',
-    jobTitle: 'null',
-    caption: 'null',
-    credit: 'null',
-    tag: ['grants', 'sbir', 'biotech'],
-    layout: 'case' 
+    res.render('sbir-grants', { 
+      title: 'Growth without dilution: Small business innovation research grants',
+      bodyClass: 'sbir-case-study',
+      summary: 'How Fuel helped tech and biotech companies win funding to bring their innovations to market',
+      name: 'null',
+      jobTitle: 'null',
+      caption: 'null',
+      credit: 'null',
+      tag: ['grants', 'sbir', 'biotech'],
+      layout: 'default' 
+    });
   });
-});
-
-// Authority blogging - thought eadership case study
-
-router.get('/thought-leadership', function(req, res, next) {
-  res.render('thought-leadership', { 
-    title: 'Healthcare content readers can trust',
-    summary: 'Growth without dilution: Small business innovation research grants',
-    name: 'null',
-    jobTitle: 'null',
-    caption: 'null',
-    credit: 'null',
-    tag: ['thought leadership', 'magazine style', 'authority'],
-    layout: 'case' 
+  
+  // Authority blogging - thought eadership case study
+  
+  router.get('/thought-leadership', function(req, res, next) {
+    res.render('thought-leadership', { 
+      title: 'Healthcare content readers can trust',
+      summary: 'Growth without dilution: Small business innovation research grants',
+      name: 'null',
+      jobTitle: 'null',
+      caption: 'null',
+      credit: 'null',
+      tag: ['thought leadership', 'magazine style', 'authority'],
+      layout: 'default' 
+    });
   });
-});
-
-// Web design case study route
-
-router.get('/web-design', function(req, res, next) {
-  res.render('web-design', { 
-    title: 'Usable, useful websites',
-    bodyClass: 'website-case-study',
-    summary: 'Web design to introduce new companies to discerning audiences',
-    name: 'null',
-    jobTitle: 'null',
-    caption: 'null',
-    credit: 'null',
-    tag: ['web design', 'prototyping', 'UI/UX'],
-    layout: 'case' 
+  
+  // Web design case study route
+  
+  router.get('/web-design', function(req, res, next) {
+    res.render('web-design', { 
+      title: 'Usable, useful websites',
+      bodyClass: 'website-case-study',
+      summary: 'Web design to introduce new companies to discerning audiences',
+      name: 'null',
+      jobTitle: 'null',
+      caption: 'null',
+      credit: 'null',
+      tag: ['web design', 'prototyping', 'UI/UX'],
+      layout: 'default' 
+    });
   });
-});
-
-router.get('/websites', function(req, res, next) {
-  res.render('websites', { 
-    title: 'Usable, useful websites',
-    bodyClass: 'website-case-study',
-    summary: 'Web design to introduce new companies to discerning audiences',
-    name: 'null',
-    jobTitle: 'null',
-    caption: 'null',
-    credit: 'null',
-    tag: ['web design', 'web copywriting','prototyping', 'UI/UX'],
-    layout: 'case' 
+  
+  router.get('/websites', function(req, res, next) {
+    res.render('websites', { 
+      title: 'Usable, useful websites',
+      bodyClass: 'website-case-study',
+      summary: 'Web design to introduce new companies to discerning audiences',
+      name: 'null',
+      jobTitle: 'null',
+      caption: 'null',
+      credit: 'null',
+      tag: ['web design', 'web copywriting','prototyping', 'UI/UX'],
+      layout: 'default' 
+    });
   });
-});
-
-
-/* CeasefirePA case study - Credit: David Scholnick
-
-router.get('/cease-fire-pa-strategy', function(req, res, next) {
-  res.render('cease-fire-pa-strategy'), {
-    title: 'Strategy to bring visibility on gun control legislation in Pennsylvania',
-    name: 'Shira Goodman',
-    jobTitle: 'Executive Director',
-    layout: 'case'
+  
+  
+  /* CeasefirePA case study - Credit: David Scholnick
+  
+  router.get('/cease-fire-pa-strategy', function(req, res, next) {
+    res.render('cease-fire-pa-strategy'), {
+      title: 'Strategy to bring visibility on gun control legislation in Pennsylvania',
+      name: 'Shira Goodman',
+      jobTitle: 'Executive Director',
+      layout: 'case'
+    });
   });
-});
-
-*/
-
+  
+  */
+  
   module.exports = router;
