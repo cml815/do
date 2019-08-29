da3f254b5dfef077a2e01c29adfed5817e068a4b
Caroline Leopold <caroline.leopold@gmail.com>
2019-08-29 12:05:30 -0400

    fixed static assets path

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* app.use('/', express.static(__dirname + '/public')); */
 app.use('/work', express.static(__dirname + '/public'));
 app.use('/projects', express.static(__dirname + '/public'));
+app.use('/about', express.static(__dirname + '/public'));
 app.use('/about/team-bios', express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
