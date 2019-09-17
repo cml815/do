a9fde23e6bb3bb9ff12df3f2295ff356d8e6074a
Caroline Leopold <caroline.leopold@gmail.com>
2019-09-16 22:40:24 -0400

    death by experiment

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -313,3 +313,37 @@ slideshow.addEventListener("touchmove", moveTouch, false);
 
 } //end slideshow
 
+const items = document.querySelectorAll('.slider-item');
+const itemCount = items.length;
+const nextItem = document.querySelector('.next');
+const previousItem = document.querySelector('.previous');
+const navItem = document.querySelector('a.toggle-nav');
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  // Check if working...
+  console.log(count);
+}
+
