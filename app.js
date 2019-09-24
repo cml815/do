70df5e4769c3a0b8aae55457512d05b90ff02d68
Caroline Leopold <caroline.leopold@gmail.com>
2019-09-24 11:12:25 -0400

    contact form

--- app.js
+++ app.js
@@ -161,11 +161,10 @@ const msg = {
 sgMail.send(msg);
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function(err, req, res, next) {
---
+++ test.js
@@ -0,0 +1,18 @@
+/* async fn https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function */
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    var result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: 'resolved'
+  }
+  
+  asyncCall();
