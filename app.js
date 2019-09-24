92a16961327d9a2a5ad0f0c1b4f8e9e2c747df52
Caroline Leopold <caroline.leopold@gmail.com>
2019-09-24 00:51:42 -0400

    sendgrid test passed

--- app.js
+++ app.js
@@ -145,10 +145,27 @@ app.post('/submit-form', function (req, res) {
   res.send("Thank you for sending us a message!")
 });
 
+// send grid test 
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'caroline@fuelcopy.com',
+  from: 'caroline@fuelcopy.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
