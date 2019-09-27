419aeccb700a51a8213ea93890dd93b3872b079e
Caroline Leopold <caroline.leopold@gmail.com>
2019-09-27 10:21:16 -0400

    rm document.write fn from footer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,32 +1,16 @@
-/* overflow test */
+/* Get year for copyright */
+
+const date = new Date().getFullYear();
+
+document.getElementById("year").innerHTML = date;
+
+
+
+/* overflow test 
 
 function isOverflown(element) {
   return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
-}
-
-/* Sticky navigation */
-
-/*
-const nav = document.querySelector('#menu');
-const navTop = nav.offsetTop;
-
-function stickyNavigation() {
-  console.log('navTop = ' + navTop);
-  console.log('scrollY = ' + window.scrollY);
-
-  if (window.scrollY >= navTop) {
-    // nav offsetHeight = height of nav
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav');
-  } else {
-    document.body.style.paddingTop = 0;
-    document.body.classList.remove('fixed-nav');
-  }
-}
-
-window.addEventListener('scroll', stickyNavigation);
-
-*/
+} */
 
 // Overlay menu //
 
